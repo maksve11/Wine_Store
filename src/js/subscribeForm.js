fb968a8c0e28b42904e537baf07a3dcd3eaa59a9
maksve11 <maks99123vell@mail.ru>
2023-09-26 13:33:06 +0300

    update code for send

--- src/js/subscribeForm.js
+++ src/js/subscribeForm.js
@@ -1,5 +1,10 @@
-document.getElementById('subscribe-button').addEventListener('click', function() {
+document.querySelector('.info-form form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var notes = document.getElementById('notes').value;
     var errorMessage = document.getElementById('error-message');
 
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -9,30 +14,33 @@ document.getElementById('subscribe-button').addEventListener('click', function()
     }
 
     var data = {
-        email: email
+        name: name,
+        email: email,
+        phone: phone,
+        notes: notes
     };
 
     var options = {
-        method: 'POST', 
-        body: JSON.stringify(data), 
+        method: 'POST',
+        body: JSON.stringify(data),
         headers: {
-            'Content-Type': 'application/json', 
+            'Content-Type': 'application/json',
         },
     };
 
     fetch('https://my-json-server.typicode.com/maksve11/Wine_Store', options)
-        .then(function(response) {
+        .then(function (response) {
             if (response.ok) {
                 return response.json();
             } else {
                 throw new Error('Ошибка при отправке данных');
             }
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
             errorMessage.textContent = 'Данные успешно отправлены.';
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.error(error);
             errorMessage.textContent = 'Произошла ошибка при отправке данных.';
         });
